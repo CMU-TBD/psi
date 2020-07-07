2bac007d972162f71e579ca746f55b7cb495cf1c
Prithu Pareek <prithu.pareek2019@gmail.com>
2020-07-07 16:49:27 -0400

    Audio pipeline first commit

---
+++ TBD/TBD.Psi.AudioPipeline/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using Microsoft.Psi;
+using Microsoft.Psi.Audio;
+using Microsoft.Psi.Speech;
+
+namespace TBD.Psi.AudioPipeline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the directory name from user input
+            Console.WriteLine("Enter the directory name >> ");
+            var directoryName = Console.ReadLine();
+            Console.WriteLine("Ok, directory name set to " + directoryName);
+
+            // Create the directory in the Audio folder
+            System.IO.Directory.CreateDirectory(@"C:\Data\Audio\" + directoryName);
+
+            using (var p = Pipeline.Create(true))
+            {
+                // create a store for the data
+                var store = Store.Create(p, "testStore", @"C:\Data\Stores");
+
+                // capture the audio from mic in wav form
+                var audioCapture = new AudioCapture(p, WaveFormat.Create16kHz1Channel16BitPcm());
+
+                // set up the audio vad
+                var audioVAD = new SystemVoiceActivityDetector(p);
+
+                // send the mic audio to the VAD for voice deteaction
+                audioCapture.Out.PipeTo(audioVAD.In);
+
+                // join the voice detection stream with audio from mic
+                var fused = audioVAD.Out.Join(audioCapture.Out);
+
+                // write the whole audiobuffer to a file called full-output.wav
+                var audioFileWriter = new WaveFileWriter(p, @"C:\Data\Audio\" + directoryName + @"\full-output.wav");
+                audioCapture.Out.PipeTo(audioFileWriter);
+
+                // init variables
+                var counter = 0;    // keeps track of number of audio files
+                var previousStateVoiceDetected = false;
+                WaveDataWriterClass writer = null;
+
+                // process the data stream containing the audio buffer and vad info
+                var process = fused.Process<(bool, AudioBuffer), (bool, AudioBuffer)>(
+                    (x, e, o) =>
+                    {
+                        // create variables for readability (extracted from message)
+                        var voiceDetected = x.Item1;
+                        var audio = x.Item2;
+
+                        // if new voice detected, create new audio file
+                        if (voiceDetected && !previousStateVoiceDetected)
+                        {
+                            Console.WriteLine("New Voice Detected ", counter);
+                            writer = new WaveDataWriterClass(new FileStream(@"C:/Data/Audio/" + directoryName + @"/output-" + counter.ToString() + @".wav", FileMode.Create), audio.Format);
+                            previousStateVoiceDetected = true;
+                        }
+                        // if same voice, continue adding to the audio file
+                        else if (voiceDetected && previousStateVoiceDetected)
+                        {
+                            previousStateVoiceDetected = true;
+                            writer.Write(audio.Data);
+                        }
+                        // if voice jus stopped than add to the counter and get rid of the writer
+                        else if (!voiceDetected && previousStateVoiceDetected)
+                        {
+                            Console.WriteLine("Voice Stoped");
+                            previousStateVoiceDetected = false;
+                            counter++;
+                            writer.Dispose();
+                        }
+                        // if no one is speaking, than do nothing
+                        else if (!voiceDetected && !previousStateVoiceDetected)
+                        {
+                            previousStateVoiceDetected = false;
+                        }
+                    });
+
+
+                // write to store for debugging
+                audioCapture.Write("Audio", store);
+                audioVAD.Write("AudioVAD", store);
+                fused.Out.Write("Joined", store);
+                p.Diagnostics.Write("D", store);
+
+                // Run the pipeline
+                p.RunAsync();
+
+                // end on key press
+                Console.WriteLine("Press any key to finish recording");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
