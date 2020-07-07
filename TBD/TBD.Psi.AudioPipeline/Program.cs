ff315ee93ce0b8014c07d2526e1f3b463dd41282
Prithu Pareek <prithu.pareek2019@gmail.com>
2020-07-07 18:09:14 -0400

    Audio split from file working

--- TBD/TBD.Psi.AudioPipeline/Program.cs
+++ TBD/TBD.Psi.AudioPipeline/Program.cs
@@ -10,6 +10,8 @@ namespace TBD.Psi.AudioPipeline
     {
         static void Main(string[] args)
         {
+
+
             // Get the directory name from user input
             Console.WriteLine("Enter the directory name >> ");
             var directoryName = Console.ReadLine();
@@ -18,26 +20,41 @@ namespace TBD.Psi.AudioPipeline
             // Create the directory in the Audio folder
             System.IO.Directory.CreateDirectory(@"C:\Data\Audio\" + directoryName);
 
+            // Get the path for the input file
+            Console.WriteLine("Please enter the path to the input file >> ");
+            var inputPath = Console.ReadLine();
+            Console.WriteLine("Ok, input file path set to " + inputPath);
+
             using (var p = Pipeline.Create(true))
             {
                 // create a store for the data
                 var store = Store.Create(p, "testStore", @"C:\Data\Stores");
 
-                // capture the audio from mic in wav form
-                var audioCapture = new AudioCapture(p, WaveFormat.Create16kHz1Channel16BitPcm());
+                // get the audio file input
+                var source = new WaveFileAudioSource(p, inputPath);
 
-                // set up the audio vad
+                // resample the audio to a different format
+                var resampler = new AudioResampler(
+                    p,
+                    new AudioResamplerConfiguration()
+                    {
+                        OutputFormat = WaveFormat.Create16kHz1Channel16BitPcm()
+                    });
+
+                source.Out.PipeTo(resampler.In);
+
+                // set up the audio 
                 var audioVAD = new SystemVoiceActivityDetector(p);
 
                 // send the mic audio to the VAD for voice deteaction
-                audioCapture.Out.PipeTo(audioVAD.In);
+                resampler.Out.PipeTo(audioVAD.In);
 
                 // join the voice detection stream with audio from mic
-                var fused = audioVAD.Out.Join(audioCapture.Out);
+                var fused = audioVAD.Out.Join(resampler.Out);
 
                 // write the whole audiobuffer to a file called full-output.wav
                 var audioFileWriter = new WaveFileWriter(p, @"C:\Data\Audio\" + directoryName + @"\full-output.wav");
-                audioCapture.Out.PipeTo(audioFileWriter);
+                resampler.Out.PipeTo(audioFileWriter);
 
                 // init variables
                 var counter = 0;    // keeps track of number of audio files
@@ -82,17 +99,13 @@ namespace TBD.Psi.AudioPipeline
 
 
                 // write to store for debugging
-                audioCapture.Write("Audio", store);
+                resampler.Write("Audio", store);
                 audioVAD.Write("AudioVAD", store);
-                fused.Out.Write("Joined", store);
+                fused.Write("Joined", store);
                 p.Diagnostics.Write("D", store);
 
                 // Run the pipeline
-                p.RunAsync();
-
-                // end on key press
-                Console.WriteLine("Press any key to finish recording");
-                Console.ReadKey();
+                p.Run();
 
             }
         }
