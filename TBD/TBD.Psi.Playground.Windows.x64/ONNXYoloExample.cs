72c1736d0088b323031e03bf7dbb4ec0b7d6c17d
Zhi Tan <zhi@zhi.fyi>
2021-04-01 18:07:18 -0400

    playground example

---
+++ TBD/TBD.Psi.Playground.Windows.x64/ONNXYoloExample.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Psi;
+using Microsoft.Psi.Onnx;
+
+namespace TBD.Psi.Playground.Windows.x64
+{
+    public class ONNXYoloExample
+    {
+        public static void Run()
+        {
+            using (var p = Pipeline.Create())
+            {
+                var store = PsiStore.Create(p, "yolo-examples", @"E:\Data\playground");
+                // create YOLO
+                var yolo = new TinyYoloV2OnnxModelRunner(p, @"E:\ONNX Models\tinyyolov2-8.onnx");
+
+                yolo.Select(m =>
+                {
+                    var rectangleList = new List<Rectangle>();
+                    foreach (var detection in m)
+                    {
+                        rectangleList.Add(Rectangle.Round(detection.BoundingBox));
+                    }
+                    return rectangleList;
+                }).Write("detection", store);
+                p.RunAsync();
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ TBD/TBD.Psi.Playground.Windows.x64/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TBD.Psi.Playground.Windows.x64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ONNXYoloExample.Run();
+        }
+    }
+}
